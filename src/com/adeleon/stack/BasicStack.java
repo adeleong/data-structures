e773bff719b04f5cb0115cdecbc240a0a0495ce0
Aderso De Leon Guzman <aderso.deleon@gmail.com>
2018-05-13 23:42:19 -0400

    new method and CardStackApp for test stack

--- src/com/adeleon/stack/BasicStack.java
+++ src/com/adeleon/stack/BasicStack.java
@@ -2,8 +2,46 @@ package com.adeleon.stack;
 
 public class BasicStack<X> {
     private X [] data;
+    private int stackPointer = 0;
 
     public BasicStack(){
         data = (X[]) new Object[1000];
     }
+
+    public void push(X newItem){
+        data[stackPointer++] = newItem;
+    }
+
+    public X pop(){
+        if(stackPointer == 0){
+            throw new IllegalStateException("No more items on the stack");
+        }
+        return data[--stackPointer];
+    }
+
+    public boolean contains(X item){
+        boolean found = false;
+        for (int i =  0; i < stackPointer; i++){
+            if (data[i].equals(item)){
+                found = true;
+                break;
+            }
+        }
+        return found;
+    }
+
+    public X access(X item){
+        while (stackPointer > 0){
+            X tmpItem = pop();
+            if (item.equals(tmpItem)){
+                return tmpItem;
+            }
+        }
+        throw new IllegalArgumentException("Could not find item on the stack");
+    }
+
+    public int size(){
+        return stackPointer;
+    }
 }
+
